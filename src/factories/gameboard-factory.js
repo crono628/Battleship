d4c1e83e18a9b7203a0217c642d515c139dd1d1c
crono628 <84817352+crono628@users.noreply.github.com>
2021-12-28 15:43:44 -0500

    Update gameboard-factory.js

--- src/factories/gameboard-factory.js
+++ src/factories/gameboard-factory.js
@@ -1,82 +1,115 @@
 import { shipFactory } from '../factories/ship-factory.js';
 
 const gameBoard = () => {
-  const board = [];
-  for (let i = 0; i < 100; i++) {
-    board.push({ hasShip: false, shotTaken: false, shipType: null, id: i });
+  let board = [];
+  let hold = [];
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      hold.push({
+        hasShip: false,
+        shotTaken: false,
+        shipName: null,
+        row: i,
+        column: j
+      });
+    }
+    board.push(hold);
+    hold = [];
   }
+
   const fleet = [];
+  const findFleetShip = (ship) => fleet.find(({ name }) => name === ship);
 
   let horizontal = true;
+
   const toggleAxis = () => (horizontal = !horizontal);
 
-  const testEdge = (coordinate, boat) => {
-    let edges = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99];
-    for (let i = 0; i < boat.length; i++) {
-      if (edges.some((edge) => coordinate + i === edge)) {
-        return true;
-      } else {
-        return false;
+  const legalMove = (xCoordinate, yCoordinate, ship) => {
+    let thisMove = true;
+    let xLengthArr = [];
+    let yLengthArr = [];
+    for (let i = 0; i < ship.length; i++) {
+      xLengthArr.push(xCoordinate + i);
+      yLengthArr.push(yCoordinate + i);
+    }
+    if (horizontal) {
+      if (xLengthArr.some((num) => num > 9)) {
+        thisMove = false;
       }
     }
-  };
-
-  const placeShip = (coordinate, boat) => {
-    let shipArray = [];
-    for (let i = 0; i < boat.length; i++) {
-      if (horizontal) {
-        board[coordinate + i].hasShip = true;
-        board[coordinate + i].shipType = boat.name;
-        shipArray.push(coordinate + i);
-      } else {
-        board[coordinate + i * 10].hasShip = true;
-        board[coordinate + i * 10].shipType = boat.name;
-        shipArray.push(coordinate + i * 10);
+    if (!horizontal) {
+      if (yLengthArr.some((num) => num > 9)) {
+        thisMove = false;
       }
     }
-    fleet.push({
-      name: boat.name,
-      location: shipArray,
-      functions: shipFactory(boat),
-    });
+    return thisMove;
   };
 
-  const findFleetShip = (ship) => fleet.find(({ name }) => name === ship);
-  const findBoardLocation = (coordinate) =>
-    board.find(({ id }) => id === coordinate);
+  const placeShip = (yRow, xColumn, boat) => {
+    if (legalMove(xColumn, yRow, boat) == true) {
+      let shipArray = [];
+      for (let i = 0; i < boat.length; i++) {
+        if (horizontal) {
+          board[yRow][xColumn + i].hasShip = true;
+          board[yRow][xColumn + i].shipName = boat.name;
+          shipArray.push([yRow, xColumn + i]);
+        } else {
+          board[yRow + i][xColumn].hasShip = true;
+          board[yRow + i][xColumn].shipName = boat.name;
+          shipArray.push([yRow + i, xColumn]);
+        }
+      }
+      fleet.push({
+        name: boat.name,
+        location: shipArray,
+        functions: shipFactory(boat)
+      });
+    } else {
+      console.log("false");
+      return false;
+    }
+  };
 
   const missedShots = [];
-  const receiveAttack = (coordinate) => {
-    const here = board[coordinate];
+
+  const receiveAttack = (yRow, xColumn) => {
+    const here = board[yRow][xColumn];
     here.shotTaken = true;
     if (here.hasShip) {
-      const ship = findFleetShip(here.shipType);
-      ship.functions.hit(ship.location.indexOf(coordinate), coordinate);
+      let ship = findFleetShip(here.shipName);
+      let { location } = ship;
+      let findIt = [yRow, xColumn];
+      let theIndex;
+      location.forEach((spot, index) => {
+        if (spot[0] === yRow && spot[1] === xColumn) {
+          theIndex = index;
+        }
+      });
+      ship.functions.hit(theIndex, yRow, xColumn);
     }
     if (!here.hasShip) {
-      missedShots.push(coordinate);
+      missedShots.push(yRow, xColumn);
     }
   };
 
   const allSunk = () => {
+    let allShipsSunk = false;
     for (let ship of fleet) {
       if (fleet.every((obj) => obj.functions.sunk())) {
-        return true;
-      } else return false;
+        allShipsSunk = true;
+      } else allShipsSunk = false;
     }
+    return allShipsSunk;
   };
 
   return {
     placeShip,
     fleet,
-    findFleetShip,
-    findBoardLocation,
     receiveAttack,
     missedShots,
     allSunk,
     toggleAxis,
-    board,
-    testEdge,
+    board
   };
 };
 
