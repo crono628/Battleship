f52782e150507c62637c1389de4acb44f7541dd6
crono628 <84817352+crono628@users.noreply.github.com>
2022-03-18 00:26:14 -0400

    initial refactor commit

--- src/factories/ship-factory.js
+++
@@ -1,9 +0,0 @@
-const shipFactory = (ship) => {
-  const health = new Array(ship.length).fill({ hitLoc: null });
-  const hit = (index, yRow, xColumn) =>
-    health.splice(index, 1, { ...health[index], hitLoc: [yRow, xColumn] });
-  const sunk = () => health.every((spot) => spot.hitLoc !== null);
-  return { hit, sunk, health };
-};
-
-export { shipFactory };
