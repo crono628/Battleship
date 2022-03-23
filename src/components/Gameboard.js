04528e76a280dc3321cfe76b6c2c886290d54c30
crono628 <84817352+crono628@users.noreply.github.com>
2022-03-23 12:03:43 -0400

    can handleWin. have not implemented turns.

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,45 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import Cell from './Cell';
-import playerFactory from '../factories/playerFactory';
+import React, { useState, useEffect } from "react";
+import Cell from "./Cell";
+import playerFactory from "../factories/playerFactory";
 
-let playerOne = playerFactory('person');
-let playerTwo = playerFactory('computer');
+let playerOne = playerFactory("person");
+let playerTwo = playerFactory("computer");
 
 const Gameboard = () => {
   const [playerOneData, setPlayerOneData] = useState(playerOne.publicBoard);
   const [playerTwoData, setPlayerTwoData] = useState(playerTwo.publicBoard);
-
+  const [gameOver, setGameOver] = useState(false);
   useEffect(() => {
     handleDisplayShips();
   });
 
+  const handleWin = () => {
+    if (playerOne.checkWin() || playerTwo.checkWin()) {
+      setGameOver(true);
+    }
+  };
+
   const handleClick = (e) => {
-    if (e.target.classList.contains('player-one')) {
+    if (e.target.classList.contains("player-one")) {
       let copy = playerOneData.slice();
       let loc = copy[e.target.id];
+
       if (loc.shotTaken) {
         setPlayerOneData(copy);
       } else if (!loc.shotTaken && loc.hasShip) {
-        e.target.style.backgroundColor = 'crimson';
+        e.target.style.backgroundColor = "crimson";
       } else {
-        e.target.style.backgroundColor = 'blue';
+        e.target.style.backgroundColor = "blue";
       }
       copy[e.target.id].shotTaken = true;
       setPlayerOneData(copy);
+      if (
+        playerOneData.filter((item) => item.hasShip && item.shotTaken)
+          .length === 17
+      ) {
+        handleWin();
+      }
       // console.log(playerOneData.filter((item) => item.shotTaken));
     }
-    if (e.target.classList.contains('player-two')) {
+    if (e.target.classList.contains("player-two")) {
       let copy = playerTwoData.slice();
       let loc = copy[e.target.id];
+
       if (loc.shotTaken) {
         setPlayerOneData(copy);
       } else if (!loc.shotTaken && loc.hasShip) {
-        e.target.style.backgroundColor = 'crimson';
+        e.target.style.backgroundColor = "crimson";
       } else {
-        e.target.style.backgroundColor = 'blue';
+        e.target.style.backgroundColor = "blue";
       }
       copy[e.target.id].shotTaken = true;
       setPlayerTwoData(copy);
+      if (
+        playerTwoData.filter((item) => item.hasShip && item.shotTaken)
+          .length === 17
+      ) {
+        handleWin();
+      }
     }
   };
 
@@ -47,8 +67,8 @@ const Gameboard = () => {
     for (const ship of playerTwoData) {
       if (ship.hasShip && !ship.shotTaken) {
         // console.log(ship);
-        let list = document.querySelectorAll('.player-two');
-        list[ship.id].style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+        let list = document.querySelectorAll(".player-two");
+        list[ship.id].style.backgroundColor = "rgba(0, 0, 0, 0.3)";
       }
     }
   }
@@ -57,7 +77,7 @@ const Gameboard = () => {
 
   let playerTwoBoard = playerTwo.publicBoard.map((item, index) => (
     <Cell
-      className={'cell player-two'}
+      className={"cell player-two"}
       id={`${item.id}`}
       key={index}
       onClick={handleClick}
@@ -66,13 +86,15 @@ const Gameboard = () => {
 
   let playerOneBoard = playerOne.publicBoard.map((item, index) => (
     <Cell
-      className={'cell player-one'}
+      className={"cell player-one"}
       id={item.id}
       key={index}
       onClick={handleClick}
     />
   ));
-  return (
+  return gameOver ? (
+    <div>game over</div>
+  ) : (
     <div className="gameboard">
       <div className="gameboard">{playerOneBoard}</div>
       <div className="gameboard">{playerTwoBoard}</div>
--- src/factories/gameboardFactory.js
+++ src/factories/gameboardFactory.js
@@ -1,7 +1,24 @@
-import shipFactory from './shipFactory';
+import shipFactory from "./shipFactory";
 
 let edges = [
-  9, 10, 19, 20, 29, 30, 39, 40, 49, 50, 59, 60, 69, 70, 79, 80, 89, 90,
+  9,
+  10,
+  19,
+  20,
+  29,
+  30,
+  39,
+  40,
+  49,
+  50,
+  59,
+  60,
+  69,
+  70,
+  79,
+  80,
+  89,
+  90
 ];
 
 const gameboardFactory = () => {
@@ -114,7 +131,7 @@ const gameboardFactory = () => {
     checkShipPlacement,
     checkWin,
     getDirection,
-    board,
+    board
   });
 };
 
